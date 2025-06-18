ebe7896f8616bc76aec8ac8aeb20add178390659
blueswayne133 <blueswayne133@gmail.com>
2025-06-18 13:56:35 -0700

    new changes

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -97,7 +97,18 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
 
-    <script src="//code.jivosite.com/widget/5C6E1NUJ5F" async></script>
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'b707eba76219e3caac742c3ee9335fe16b750169';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+    </script>
+    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 
 </head>
 
--- resources/views/user/header.blade.php
+++ resources/views/user/header.blade.php
@@ -154,7 +154,18 @@
             border-color: #049dff;
         }
     </style>
-    <script src="//code.jivosite.com/widget/5C6E1NUJ5F" async></script>
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'b707eba76219e3caac742c3ee9335fe16b750169';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+    </script>
+    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 
 </head>
 
